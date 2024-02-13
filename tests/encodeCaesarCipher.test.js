9d4d09be2ce364bc141a8e8fbac33f741aaf09a9
Renato Cipolla <renato.cipolla94@gmail.com>
2024-02-13 14:25:06 +0000

    add more tests to encodeCaesarCipher

--- tests/encodeCaesarCipher.test.js
+++ tests/encodeCaesarCipher.test.js
@@ -7,7 +7,7 @@ describe("encodeCaesarCipher", () => {
     const actual = encodeCaesarCipher(input, 5);
     expect(actual).toBe(expected);
   });
-  test("when passed a single word and 0, it should return the same word", () => {
+  test("when passed 0, it should return the same string", () => {
     const input = "hello";
     const expected = "hello";
     const actual = encodeCaesarCipher(input, 0);
@@ -15,7 +15,25 @@ describe("encodeCaesarCipher", () => {
   });
   test("when passed a single word, it should return the word shifted by the provided number", () => {
     const input = "hello";
-    const expected = "mjqqt";
+    const expected = "jgnnq";
+    const actual = encodeCaesarCipher(input, 2);
+    expect(actual).toBe(expected);
+  });
+  test("should handle different letter case", () => {
+    const input = "Hello";
+    const expected = "Khoor";
+    const actual = encodeCaesarCipher(input, 3);
+    expect(actual).toBe(expected);
+  });
+  test("should handle punctuation and symbols", () => {
+    const input = "[Hello!]";
+    const expected = "[Lipps!]";
+    const actual = encodeCaesarCipher(input, 4);
+    expect(actual).toBe(expected);
+  });
+  test("when passed multiple words, it should return the string shifted by the provided number", () => {
+    const input = "Hello World! This is a test.";
+    const expected = "Mjqqt Btwqi! Ymnx nx f yjxy.";
     const actual = encodeCaesarCipher(input, 5);
     expect(actual).toBe(expected);
   });
